fcecb7b3def279ae202e3609798ad43305336cf8
wykzimme <bryce.e.zimmerman@gmail.com>
2025-01-25 14:05:13 -0700

    Update Drive.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -35,8 +35,6 @@ public class Drive extends OpMode {
         driveteleop.backleft.setPower(gamepad1.left_stick_y / 1.1 + -gamepad1.right_stick_x * 1.1 -gamepad1.left_stick_x);
         driveteleop.topright.setPower(-gamepad1.left_stick_y * 1.1 + gamepad1.right_stick_x * 1.2 -gamepad1.left_stick_x);
         driveteleop.backright.setPower(gamepad1.left_stick_y * 1.1 + gamepad1.right_stick_x * 1.1 + gamepad1.left_stick_x);
-        LineraMecanizmeteleop.leftliner.setPower(gamepad2.right_stick_y);
-
         LineraMecanizmeteleop.arm.setPower(gamepad2.left_stick_y);
 
         if (gamepad2.y){
